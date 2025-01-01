a330c0a17497c8b6b1e370b12be3b2c41fb0bd11
Hui Yi Lim <hui-lim@c2r4s1.42singapore.sg>
2025-01-01 18:09:10 +0800

    add

--- traversetree.c
+++ traversetree.c
@@ -130,3 +130,17 @@ int checkif_builtin(char* cmd)
 		return (printf("%s\n", "run exit"), 1);
 	return (0);
 }
+
+int	ft_strcmp(char *str1, char *str2)
+{
+	int	i;
+
+	i = 0;
+	while (str1[i] != '\0')
+	{
+		if (str1[i] != str2[i] || str2[i] == '\0')
+			return (-1);
+		i++;
+	}
+	return (1);
+}
